6590a88eac4a260c1153b69d6de9b446c3179a6e
Amarjeet-ka <116912077+Amarjeet-ka@users.noreply.github.com>
2023-05-21 20:09:07 +0530

    Fouth commit

--- server/models/gridimage.js
+++ server/models/gridimage.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   sno: { type: Number, required: true },
-  image: { type: String, required: false, unique: true },
+  image: { type: String, required: false, unique: false },
   name: { type: String, required: true },
   description: { type: String },
   category: { type: String },
--- server/routes/gridimage.js
+++ server/routes/gridimage.js
@@ -1,8 +1,34 @@
 const express = require("express");
-
+const {GridFsStorage} = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
 const router = express.Router();
+const multer = require('multer');
 const Gridimage = require("../models/gridimage");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://registration:registration@cluster0.sd2bhdx.mongodb.net/?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+const conn = mongoose.connection;
+
+// Initialize GridFS
+let gfs;
+conn.once('open', () => {
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
+
+// Create GridFS storage engine
+const storage = new GridFsStorage({
+  url:"mongodb+srv://registration:registration@cluster0.sd2bhdx.mongodb.net/?retryWrites=true&w=majority",
+  file: (req, file) => {
+    return {
+      filename: file.originalname,
+    };
+  },
+});
+const upload = multer({ storage });
 
 router.post('/gridimage_post', async (req, res) => {
   try {
@@ -21,6 +47,43 @@ router.get("/gridimage_get", async (req, res) => {
     res.status(500).send(err);
   }
 });
+
+router.post('/users', upload.single('image'), async (req, res) => {
+  const { sno, name, description, category } = req.body;
+
+  // Create a new Gridimage document
+  const newGridImage = new Gridimage({
+    sno,
+    image: req.file.filename,
+    name,
+    description,
+    category,
+  });
+
+  try {
+    // Save the user to the database
+    const savedUser = await newGridImage.save();
+    res.status(200).json(savedUser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Get a user by ID
 router.get("/users/:id", (req, res) => {
